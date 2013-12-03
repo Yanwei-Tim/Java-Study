7256e9b13c62d297cd182869596fe462e09b134a
zhuifeng <zhuifeng1017@gmail.com>
2013-12-03 11:12:47 +0800

    jni编译例子实现

---
+++ JDll/JNI_Dll.cpp
@@ -0,0 +1,19 @@
+#include "JNI_Dll.h"
+#include <malloc.h>
+
+JNIEXPORT void JNICALL Java_Test_showVoidContent(JNIEnv *env, jclass cls){
+
+}
+
+ JNIEXPORT jstring  JNICALL Java_Test_getOneContent
+(JNIEnv *env, jclass cls, jstring str1, jstring str2)
+{
+	const char *szStr1 = env->GetStringUTFChars(str1, 0);
+	const char *szStr2 = env->GetStringUTFChars(str2, 0);
+	int len = strlen(szStr1) + strlen(szStr2);
+	char *buff = (char*)alloca(len+1);
+	memcpy(buff, szStr1, strlen(szStr1));
+	memcpy(buff+strlen(szStr1), szStr2, strlen(szStr2));
+	buff[len] = '\0';
+	return env->NewStringUTF(buff);
+}
