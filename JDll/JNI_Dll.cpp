6c7091a4d4272c0b9bcbac4426c8b9d07c3af2c8
zhuifeng <zhuifeng1017@gmail.com>
2013-12-03 11:21:39 +0800

    add java test file

--- JDll/JNI_Dll.cpp
+++ JDll/JNI_Dll.cpp
@@ -10,10 +10,11 @@ JNIEXPORT void JNICALL Java_Test_showVoidContent(JNIEnv *env, jclass cls){
 {
 	const char *szStr1 = env->GetStringUTFChars(str1, 0);
 	const char *szStr2 = env->GetStringUTFChars(str2, 0);
-	int len = strlen(szStr1) + strlen(szStr2);
+	int len = strlen(szStr1) + strlen(szStr2)+ strlen("~ test OK");
 	char *buff = (char*)alloca(len+1);
 	memcpy(buff, szStr1, strlen(szStr1));
 	memcpy(buff+strlen(szStr1), szStr2, strlen(szStr2));
+	memcpy(buff+strlen(szStr1)+strlen(szStr2), "~ test OK", strlen("~ test OK"));
 	buff[len] = '\0';
 	return env->NewStringUTF(buff);
 }
