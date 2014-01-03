83c5d2fc36faf250115c6a3c68700c84daeda19e
zhuifeng <zhuifeng1017@gmail.com>
2014-01-03 17:13:08 +0800

    【Update】 ibatis TransactionManager two datasource

--- SSI/src/com/ssi/dao/ContactDao.java
+++ SSI/src/com/ssi/dao/ContactDao.java
@@ -3,6 +3,7 @@
  */
 package com.ssi.dao;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
@@ -30,22 +31,37 @@ public class ContactDao extends SqlMapClientUtil {
 
 	public int insert(Contact contact) throws Exception {
 		int retId = 0;
+		Connection conn2 = sqlMapClient2.getDataSource().getConnection();
+		Connection conn = sqlMapClient.getDataSource().getConnection();
+		boolean flag2 = conn2.getAutoCommit();
+		boolean flag = conn.getAutoCommit();
 		try {
-			sqlMapClient2.startTransaction();
-			sqlMapClient.startTransaction();
+			conn.setAutoCommit(false);
+			conn2.setAutoCommit(false);
+			sqlMapClient2.setUserConnection(conn2);
+			sqlMapClient.setUserConnection(conn);
+			
+//			sqlMapClient2.startTransaction();
+//			sqlMapClient.startTransaction();
 			
 			int lastId = (int) sqlMapClient2.insert("Contact.insert", contact);
 			System.out.println("insert lastId:" + lastId);
 			
 			retId = (int) sqlMapClient.insert("Contact.insert", contact);
 			
-			sqlMapClient.commitTransaction();
-			sqlMapClient2.commitTransaction();
+			conn.commit();
+			conn2.commit();
+//			sqlMapClient.commitTransaction();
+//			sqlMapClient2.commitTransaction();
 		} catch (SQLException e) {
 			System.out.println("insert exception:" + e);
+			conn.rollback();
+			conn2.rollback();
 		}finally{
-			sqlMapClient.endTransaction();
-			sqlMapClient2.endTransaction();
+//			sqlMapClient.endTransaction();
+//			sqlMapClient2.endTransaction();
+			conn.setAutoCommit(flag);
+			conn2.setAutoCommit(flag2);
 		}
 		return retId;
 	}
