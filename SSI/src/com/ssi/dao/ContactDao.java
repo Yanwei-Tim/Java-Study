21c472779f79dba01002622d7853c9bf1ccac783
zhuifeng <zhuifeng1017@gmail.com>
2014-01-03 15:36:14 +0800

    [ADD] two ibatis DataSource

--- SSI/src/com/ssi/dao/ContactDao.java
+++ SSI/src/com/ssi/dao/ContactDao.java
@@ -6,8 +6,11 @@ package com.ssi.dao;
 import java.util.List;
 import java.util.Map;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Repository;
 
+import com.ibatis.sqlmap.client.SqlMapClient;
 import com.ssi.bean.Contact;
 
 /**
@@ -17,11 +20,16 @@ import com.ssi.bean.Contact;
 @Repository
 public class ContactDao extends SqlMapClientUtil {
 	
+	@Resource(name = "sqlMapClient2")
+	protected SqlMapClient sqlMapClient2;
+	
 	public Map selectById(int id) throws Exception {
 		return (Map) sqlMapClient.queryForObject("Contact.getById", Integer.valueOf(id));
 	}
 
 	public int insert(Contact contact) throws Exception {
+		int lastId = (int) sqlMapClient2.insert("Contact.insert", contact);
+		System.out.println("---sqlMapClient2 insert return : " + lastId);
 		return (int) sqlMapClient.insert("Contact.insert", contact);
 	}
 	
