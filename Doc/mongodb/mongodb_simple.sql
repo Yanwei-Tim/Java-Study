fbaf9335297c42a6d94b617a82a87a1bed7e4a7b
zhuifeng <zhuifeng1017@gmail.com>
2014-04-13 15:03:08 +0800

    linux shell

--- Doc/mongodb/mongodb_simple.sql
+++ Doc/mongodb/mongodb_simple.sql
@@ -1,1 +1,3 @@
-db.deviceLocationMongo.find({"imei":"863359010048193"}).sort({create_time:-1}).limit(10)
+db.deviceLocationMongo.find({"log_category":2}).sort({create_time:-1}).limit(10)
+
+db.log.find({"log_category":2}).sort({create_time:-1})
